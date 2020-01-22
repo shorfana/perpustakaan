9192f86caad85d23d0ef5a666e49ebcb995f3b4c
Erzi Hutama Dwirama Putra <erzihutama@gmail.com>
2020-01-22 12:38:51 +0700

    update front

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -21,6 +21,46 @@ class Home extends CI_Controller{
 
   }
 
+  
+  function about()
+  {
+
+
+    $this->load->view('front/header');
+    $this->load->view('front/index');//melempar data dari view
+    $this->load->view('front/footer');
+
+  }
+
+  
+  function book()
+  {
+
+
+    $this->load->view('front/header');
+    $this->load->view('front/index');//melempar data dari view
+    $this->load->view('front/footer');
+
+  }
+  
+  function books()
+  {
+
+
+    $this->load->view('front/header');
+    $this->load->view('front/books');//melempar data dari view
+    $this->load->view('front/footer');
+
+  }
+  
+  function kategori()
+  {
+    $this->load->view('front/header');
+    $this->load->view('front/kategori');//melempar data dari view
+    $this->load->view('front/footer');
+
+  }
+
   // function haha(){
   //   $this->load->view('front/header');
   //   $this->load->view('front/index');//melempar data dari view
